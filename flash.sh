5cd2b8161802244b54a94391d6a9ce4b91369c35
虎子哥 <flashindream@gmail.com>
2024-09-02 09:57:44 +0800

    update flash

--- flash.sh
+++ flash.sh
@@ -1,5 +1,12 @@
 if [ $1 ]
 then
 cd ./project
-sudo make flash COMX=$1 
+uNames=`uname -s`
+osName=${uNames: 0: 4}
+if [ "$osName" == "MING" ] # Darwin
+then
+make flash COMX=$1 
+else
+sudo make flash COMX=$1
+fi
 fi
